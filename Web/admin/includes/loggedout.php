27af540f1d4624409e8e259fed4fb1df8e4d018a
adriantam18 <adrian.tamayo39@myhunter.cuny.edu>
2016-09-20 03:19:29 -0400

    Currently not needed

--- Web/admin/includes/loggedout.php
+++
@@ -1,8 +0,0 @@
-
- <div id="main_body">
-            	<div id="content"> You are on the administration side.
-                    <div id="login">
-                        <p> You have successfully logged out. </p>
-                    </div>
-                </div>
- </div>
