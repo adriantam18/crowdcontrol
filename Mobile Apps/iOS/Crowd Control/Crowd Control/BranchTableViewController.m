00b0a5beb56c04d408a74526e1e7ac35fdb961f0
adriantam18 <adrian.tamayo39@myhunter.cuny.edu>
2016-09-24 15:40:15 -0400

    Changed request url and response data

--- Mobile Apps/iOS/Crowd Control/Crowd Control/BranchTableViewController.m
+++ Mobile Apps/iOS/Crowd Control/Crowd Control/BranchTableViewController.m
@@ -17,8 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Encode string for URL 
+
+    // Encode string for URL
     NSCharacterSet *set = [NSCharacterSet URLQueryAllowedCharacterSet];
     self.company = [self.company stringByAddingPercentEncodingWithAllowedCharacters:set];
     [self requestDataFromAPI];
@@ -32,19 +32,19 @@
 // Request data from the API
 - (void)requestDataFromAPI {
     // Set up URL for API call
-    NSString *urlString = [NSString stringWithFormat:@"https://crowdcontrol-adriantam18.rhcloud.com/requests.php/?data=branch&comp=%@",self.company];
-    
+    NSString *urlString = [NSString stringWithFormat:@"https://crowdcontrol-adriantam18.rhcloud.com/api/v1/branches/?company=%@",self.company];
+
     NSURL *URL = [NSURL URLWithString:urlString];
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager GET:URL.absoluteString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        // Retrieve data and reload the table 
-        self.branches = [responseObject objectForKey:@"branches"];
+        // Retrieve data and reload the table
+        self.branches = [responseObject objectForKey:@"data"];
         [self.tableView reloadData];
-        
+
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         // Report any error to user with an alert
         NSLog(@"Error: %@", error);
-        
+
         if ([[[error userInfo] objectForKey:AFNetworkingOperationFailingURLResponseErrorKey] statusCode] != 404) {
             UIAlertController *alertController = [UIAlertController
                                                   alertControllerWithTitle:@"Error"
@@ -70,47 +70,48 @@
         UITableViewCell *selectedCell = [self.tableView cellForRowAtIndexPath:savedSelection];
         for(int i = 0; i < [self.branches count]; i++) {
             if (self.branches[i][@"address"] == selectedCell.textLabel.text) {
-                
+
                 // Pass company name and address to next view
-                roomController.company = self.branches[i][@"company_name"];
+                roomController.branchId = sef.branches[i][@"branch_id"]
                 roomController.address = self.branches[i][@"address"];
-                
-                
+                roomController.company = self.company;
+
+
                 // Current time split into hours and minutes
                 NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:[NSDate date]];
                 NSInteger currHr = [components hour];
                 NSInteger currtMin = [components minute];
-                
+
                 // Get opening and closing times
-                NSString *openTime = self.branches[i][@"opening_hours"];
-                NSString *closeTime = self.branches[i][@"closing_hours"];
-                
+                NSString *openTime = self.branches[i][@"open_hours"];
+                NSString *closeTime = self.branches[i][@"close_hours"];
+
                 // Split opening and closing hours into hours and minutes
                 int opHr = [[[openTime componentsSeparatedByString:@":"] objectAtIndex:0] intValue];
                 int opMin = [[[openTime componentsSeparatedByString:@":"] objectAtIndex:1] intValue];
                 int clHr = [[[closeTime componentsSeparatedByString:@":"] objectAtIndex:0] intValue];
                 int clMin = [[[closeTime componentsSeparatedByString:@":"] objectAtIndex:1] intValue];
-                
-                
+
+
                 // Form single value time
                 int opTime = (opHr * 60) + opMin;
                 int clTime = (clHr * 60) + clMin;
-                
+
                 long timeNow = (currHr * 60) + currtMin;
-                
+
                 // Determine if it is open and then pass it on to next view
                 if(timeNow >= opTime && timeNow <= clTime) {
                     roomController.open = YES;
                 } else {
                     roomController.open = NO;
                 }
-                
-                
-                
-                
+
+
+
+
             }
         }
-        
+
     }
 }
 
@@ -127,16 +128,16 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
+
     static NSString *CellIdentifier = @"Branch Cell";
     UITableViewCell *cell = [tableView
                              dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    
-    
+
+
+
     cell.textLabel.text=[self.branches objectAtIndex:indexPath.row][@"address"];
-    
-    
+
+
     return cell;
 }
 
