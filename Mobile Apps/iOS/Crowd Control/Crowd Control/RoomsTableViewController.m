b2d91c997e0226156bdadc15b19c3ac8f80d1bbb
adriantam18 <adrian.tamayo39@myhunter.cuny.edu>
2016-09-24 15:41:24 -0400

    Changed request url and response data

--- Mobile Apps/iOS/Crowd Control/Crowd Control/RoomsTableViewController.m
+++ Mobile Apps/iOS/Crowd Control/Crowd Control/RoomsTableViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     // Encode strings for URL
     NSCharacterSet *set = [NSCharacterSet URLQueryAllowedCharacterSet];
     self.company = [self.company stringByAddingPercentEncodingWithAllowedCharacters:set];
@@ -33,16 +33,16 @@
 // Request data from the API
 - (void)requestDataFromAPI {
     // Set up URL for the API call
-    NSString *urlString = [NSString stringWithFormat:@"https://crowdcontrol-adriantam18.rhcloud.com/requests.php/?data=room&comp=%@&branch=%@",self.company, self.address];
-    
+    NSString *urlString = [NSString stringWithFormat:@"https://crowdcontrol-adriantam18.rhcloud.com/api/v1/room/?branch_id=%@",self.branchId];
+
     NSURL *URL = [NSURL URLWithString:urlString];
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager GET:URL.absoluteString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        
+
         // Retrieve data and reload data into the table
-        self.rooms = [responseObject objectForKey:@"rooms"];
+        self.rooms = [responseObject objectForKey:@"data"];
         [self.tableView reloadData];
-        
+
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         // Report any error to user with an alert
         NSLog(@"Error: %@", error);
@@ -70,16 +70,16 @@
         NSIndexPath *savedSelection = self.tableView.indexPathForSelectedRow;
         UITableViewCell *selectedCell = [self.tableView cellForRowAtIndexPath:savedSelection];
         for(int i = 0; i < [self.rooms count]; i++) {
-            if (self.rooms[i][@"room"] == selectedCell.textLabel.text) {
+            if (self.rooms[i][@"room_number"] == selectedCell.textLabel.text) {
                 // Pass data to next view
-                roomController.company = self.rooms[i][@"company"];
-                roomController.address = self.rooms[i][@"address"];
-                roomController.capacity = self.rooms[i][@"max_capacity"];
-                roomController.room = self.rooms[i][@"room"];
+                roomController.company = self.company;
+                roomController.address = self.address;
+                roomController.room = self.rooms[i][@"room_number"];
+                roomController.roomId = self.rooms[i][@"room_id"];
                 roomController.open = self.open;
             }
         }
-        
+
     }
 }
 
@@ -99,9 +99,9 @@
     static NSString *CellIdentifier = @"Rooms Cell";
     UITableViewCell *cell = [tableView
                              dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    cell.textLabel.text= [self.rooms objectAtIndex:indexPath.row][@"room"];
-    
+
+    cell.textLabel.text= [self.rooms objectAtIndex:indexPath.row][@"room_number"];
+
     return cell;
 }
 
